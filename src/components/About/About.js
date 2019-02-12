aab5183a41701fc57a958a8ebfb9ba1786d8c3b2
Kevin Ung <ung.kevin78@gmail.com>
2019-02-12 16:12:21 -0700

    afternoon final with black diamond

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
+import { Link, Switch, Route } from 'react-router-dom';
+import History from '../History/History';
+import Contact from '../Contact/Contact';
 
 export default class About extends Component {
   render() {
     return (
       <div>
         <div className='subnav'>
+          <Link to='/about' className='subnav'>
+            <h3>About</h3>
+          </Link>
+
+          <Link to='/about/history' className='subnav'>
+            <h3>History</h3>
+          </Link>
+
+          <Link to='/about/contact' className='subnav'>
+            <h3>Contact</h3>
+          </Link>
+
         </div>
         <div className='box'>
+        <Switch>
+          <Route path="/about/history" component={History}/>
+          <Route path="/about/contact" component={Contact}/>
+          <Route exact path="/about" render={() =>(
+            <div>
+              <h1>About the University</h1>
+              <p>Lorem ipsum</p>
+            </div>
+          )}/>
+        </Switch>
         </div>
       </div>
+      
+
     )
   }
 }
--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class ClassList extends Component {
   constructor() {
-    super()
-    
+    super();
+    this.state = {
+      students: []
+    }
+  }
+
+  componentDidMount() {
+    console.log(this.props)
+    axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`).then(res => {
+      console.log(222, res)
+      this.setState({
+        students: res.data
+      })
+    })
   }
 
   render() {
+    const students = this.state.students.map((student, i) => {
+      return (
+        <Link key={i} to={`/student/${student.id}`}>
+          <h3>{student.first_name} {student.last_name} </h3>
+        </Link>
+      )
+    })
+
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
+        {students}
+        <button onClick={()=>this.props.history.goBack()}> Back to Home </button>
 
       </div>
     )
